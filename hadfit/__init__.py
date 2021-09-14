fe8eeb698d3eacb6b4f06d326499a4e0147b3abb
Sergio Chaves Garcia-Mascaraque <sergiozteskate@gmail.com>
2021-09-14 10:42:43 +0000

    Updated modules

--- hadfit/__init__.py
+++ hadfit/__init__.py
@@ -1,1 +1,25 @@
 __version__ = '0.1.0'
+
+# -- Load the Hadron related objects
+from .hadron import Hadron
+from .hadron import Flavour
+from .hadron import Channel
+from .hadron import hadron_from_fastsum
+from .hadron import cosh_ansatz
+from .hadron import exp_ansatz
+
+# -- Load the Model related objects
+from .model import Model
+from .model import CompositeModel
+from .model import bootstrap_fit
+from .model import generate_multistate
+from .model import generate_cosh_model
+from .model import generate_exp_model
+
+# -- Load the MultiStateFit objects
+from .msfit import MultiStateFit
+
+# -- Load some utility functions
+from .analysis import FastsumRetriever
+from .analysis import tidy_fastsum
+from .analysis import save_fastsum
