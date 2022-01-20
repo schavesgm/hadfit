fba9942b0908adbd69cf93f29665aadc4da46815
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-01-20 11:48:06 +0000

    Updated compute ratio script with more command line arguments

--- scripts/compute_ratio.py
+++ scripts/compute_ratio.py
@@ -87,6 +87,8 @@ if __name__ == '__main__':
     # Add some data to the parser
     parser.add_argument('-s', '--sources', type=str, choices=['ll', 'ss'], help='Sources used in the calculation')
     parser.add_argument('-p', '--path', type=str, default='./data', help='Sources used in the calculation')
+    parser.add_argument('-i', '--include_128', action='store_true', help='Compute the ratio of Nt=128')
+    parser.add_argument('-t', '--use_tau_tilde', action='store_true', help='Use tau_tilde = t - Nt / 2 in the plot')
 
     # Parse the command line arguments
     args = parser.parse_args()
@@ -94,27 +96,32 @@ if __name__ == '__main__':
     # Assert the path exists
     assert os.path.exists(args.path), f'{args.path=} does not exist in system'
 
-    # Sources used in the calculation
-    sources, path = args.sources, args.path
+    # Get some variables from the command line parser
+    sources, path, include_128, use_tau_tilde = args.sources, args.path, args.include_128, args.use_tau_tilde
 
     # Color palette used in the calculation
-    COLORS = (
-        '#00043a', '#212f45',  # 128 - 64
-        '#577590', '#34a0a4',  # 56 - 48
-        '#76c893', '#495057',  # 40 - 36
-        '#f9c74f', '#f9844a',  # 32 - 28
-        '#f3722c', '#f94144',  # 24 - 20
-    )
+    COLORS = [
+        '#212f45', '#577590', '#34a0a4',  # 64 - 56 - 48
+        '#76c893', '#495057', '#f9c74f',  # 40 - 36 - 32
+        '#f9844a', '#f3722c', '#f94144',  # 28 - 24 - 20
+    ]
 
-    # Get all needed files in the data directory
-    files = filter(lambda p: p.name.endswith(f'_{sources}'), Path(args.path).iterdir())
+    # Filter used in the files
+    ffunc = lambda p : p.name.endswith(f'_{sources}') and ('128x32' not in p.name if not include_128 else True)
 
-    # Sort the files from lower to higher temperature
-    files = sorted(files, key=lambda p: int(p.name.split('x')[0]))[::-1]
+    # Prepend the colour for Nt=128 if included
+    if include_128: COLORS.insert(0, '#00043a')
+
+    # Get all needed files in the data directory sorted from low to high temperature
+    files = sorted(filter(ffunc, Path(args.path).iterdir()), key=lambda p: int(p.name.split('x')[0]))[::-1]
 
     # x-label and y-label strings
-    xlabel = r'$\tau / N_\tau$'
-    ylabel = r'$R(\tau) = \frac{V(\tau) \cdot A(N_\tau/2)}{A(\tau) \cdot V(N_\tau/2)}$'
+    if not use_tau_tilde:
+        xlabel = r'$\tau / N_\tau$'
+        ylabel = r'$R(\tau) = \frac{V(\tau) \cdot A(N_\tau/2)}{A(\tau) \cdot V(N_\tau/2)}$'
+    else:
+        xlabel = r'$\tilde{\tau} = \tau - \frac{N_\tau}{2}$'
+        ylabel = r'$R(\tilde{\tau}) = \frac{V(\tilde{\tau}) \cdot A(N_\tau/2)}{A(\tilde{\tau}) \cdot V(N_\tau/2)}$'
 
     # Folder where the figures will be stored
     if not os.path.exists('./figures/'): os.makedirs('./figures')
@@ -146,11 +153,13 @@ if __name__ == '__main__':
             # Compute the ratio statistic
             ratio = compute_ratio(vec, axp, Nb=500)
 
+            # Generate the x-axis data
+            t_data = np.arange(vec.Nk) / vec.Nk if not use_tau_tilde else np.arange(vec.Nk) - vec.Nk // 2
+
             # Plot the data in the axis
             axis.errorbar(
-                np.arange(vec.Nk) / vec.Nk, ratio.pe, ratio.se, 
-                color=COLORS[f], label=fr'$N_\tau={vec.Nk}$',
-                alpha=1.0 if vec.Nk < 128 else 0.3
+                t_data, ratio.pe, ratio.se, color=COLORS[f], 
+                label=fr'$N_\tau={vec.Nk}$', alpha=1.0 if vec.Nk < 128 else 0.3
             )
 
             # Compute the 0.4 and 0.6 point in the dataset
@@ -162,41 +171,44 @@ if __name__ == '__main__':
                 max_val = max(max_val, max(ratio.pe[point_40:point_60]))
 
         # Load the lowest temperature data
-        vec = FastsumRetriever('./output/', flavour, 'gi', sources, prop=1.0)
-        axv = FastsumRetriever('./output/', flavour, 'gig5', sources, prop=0.7)
+        vec_data = FastsumRetriever('./output/', flavour, 'gi', sources, nk_or_prop='p1.0')
+        axv_data = FastsumRetriever('./output/', flavour, 'gig5', sources, nk_or_prop='p0.7')
 
         # Get the lowest temperature median estimates
-        vec, axv = vec.get_median_estimates()[128], axv.get_median_estimates()[128]
+        M0_vec, M0_axv = vec_data.get_median_estimates()[128], axv_data.get_median_estimates()[128]
 
         # Assume normality to obtain the standard errors in the measurements
-        se_vec = max(abs(vec[0] - vec[1]), abs(vec[1] - vec[2])) / 1.96
-        se_axv = max(abs(axv[0] - axv[1]), abs(axv[1] - axv[2])) / 1.96
+        se_vec = max(abs(M0_vec[0] - M0_vec[1]), abs(M0_vec[1] - M0_vec[2])) / 1.96
+        se_axv = max(abs(M0_axv[0] - M0_axv[1]), abs(M0_axv[1] - M0_axv[2])) / 1.96
 
         # Get the difference of squared masses
-        delta_M_pe = (vec[1] ** 2 - axv[1] ** 2)
+        delta_M_pe = (M0_vec[1] ** 2 - M0_axv[1] ** 2)
 
         # Function to generate a new random estimate of delta_M
-        gen_dM = lambda: (vec[1] + np.random.normal(0.0, scale=se_vec)) ** 2 - \
-                         (axv[1] + np.random.normal(0.0, scale=se_axv)) ** 2
+        gen_dM = lambda: (M0_vec[1] + np.random.normal(0.0, scale=se_vec)) ** 2 - \
+                         (M0_axv[1] + np.random.normal(0.0, scale=se_axv)) ** 2
 
         # Estimate the uncertainties by propagating using MonteCarlo
         delta_M_se = np.std([gen_dM() for _ in range(5000)], ddof=1)
 
         # Plot the curve using the fitted data
-        t = np.linspace(0, 1, 500)
+        t    = np.linspace(0, 1, 500) if not use_tau_tilde else np.arange(128) - 128 // 2
+        t_sq = 5997 * (t - 0.5) ** 2 if not use_tau_tilde else t ** 2
 
         # Generate the central value and its standard error
-        central = 1 + 0.5 * delta_M_pe * 5997 * ((t - 0.5) ** 2)
-        std_err = np.std(
-            np.vstack([1 + 0.5 * gen_dM() * 5997 * ((t - 0.5) ** 2) for _ in range(5000)]), ddof=1, axis=0
-        )
+        central = 1 + 0.5 * delta_M_pe * t_sq
+        std_err = np.std(np.vstack([1 + 0.5 * gen_dM() * t_sq for _ in range(5000)]), ddof=1, axis=0)
 
         # Add the data to the axis
         axis.plot(t, central, color='#7b2cbf', label='Analytic')
         axis.fill_between(t, central - 1.96 * std_err, central + 1.96 * std_err, color='#7b2cbf', alpha=0.3)
 
         # Restrict the limits in the x axis and y axis
-        axis.set_xlim(0.4, 0.6), axis.set_ylim(0.97 * min_val, 1.02 * max_val)
+        axis.set_ylim(0.97 * min_val, 1.02 * max_val)
+        if not use_tau_tilde: 
+            axis.set_xlim(0.4, 0.6)
+        else:
+            axis.set_xlim(-20, 20)
 
         # Get the legend
         handles, labels = axis.get_legend_handles_labels()
@@ -219,4 +231,4 @@ if __name__ == '__main__':
         fig.suptitle(f'Flavour={flavour.name} - Sources={sources}')
 
         # Save the figure in the corresponding path
-        fig.savefig(f'./figures/ratioAV_{flavour}_{sources}.pdf')
+        fig.savefig(f'./figures/ratioAV_{flavour}_{sources}_i{int(include_128)}_t{int(use_tau_tilde)}.pdf')
